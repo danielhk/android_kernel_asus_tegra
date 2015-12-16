957ec49e7cde1f8a4f7bbda9a6e67d66b4a4eda0
Lorenzo Colitti <lorenzo@google.com>
2015-12-16 12:30:03 +0900

    net: diag: Add the ability to destroy a socket.
    This patch adds a SOCK_DESTROY operation, a destroy function
    pointer to sock_diag_handler, and a diag_destroy function
    pointer.  It does not include any implementation code.
    
    [Backport of net-next 64be0aed59ad519d6f2160868734f7e278290ac1]
    
    Change-Id: I1d998e1c5f836b2f5638c0f79244c372c8d2d9d9
    Signed-off-by: Lorenzo Colitti <lorenzo@google.com>
    Acked-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/sock_diag.h
+++ include/linux/sock_diag.h
@@ -2,6 +2,7 @@
 #define __SOCK_DIAG_H__
 
 #define SOCK_DIAG_BY_FAMILY 20
+#define SOCK_DESTROY_BACKPORT 21
 
 struct sk_buff;
 struct nlmsghdr;
@@ -14,6 +15,7 @@ struct sock_diag_req {
 struct sock_diag_handler {
 	__u8 family;
 	int (*dump)(struct sk_buff *skb, struct nlmsghdr *nlh);
+	int (*destroy)(struct sk_buff *skb, struct nlmsghdr *nlh);
 };
 
 int sock_diag_register(struct sock_diag_handler *h);
@@ -21,6 +23,13 @@ void sock_diag_unregister(struct sock_diag_handler *h);
 
 void sock_diag_register_inet_compat(int (*fn)(struct sk_buff *skb, struct nlmsghdr *nlh));
 void sock_diag_unregister_inet_compat(int (*fn)(struct sk_buff *skb, struct nlmsghdr *nlh));
+
+int sock_diag_check_cookie(void *sk, __u32 *cookie);
+void sock_diag_save_cookie(void *sk, __u32 *cookie);
+
+int sock_diag_put_meminfo(struct sock *sk, struct sk_buff *skb, int attr);
+
+int sock_diag_destroy(struct sock *sk, int err);
 
 extern struct sock *sock_diag_nlsk;
 #endif
